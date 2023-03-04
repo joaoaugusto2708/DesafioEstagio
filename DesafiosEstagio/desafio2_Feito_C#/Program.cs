c60055a19411e0df928d0b55b8ceb1824b6243f4
João Augusto <joaorodriguesjf@hotmail.com>
2023-03-04 02:25:23 -0300

    Resolvidos

---
+++ DesafiosEstagio/desafio2_Feito_C#/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        fibonachi();
+    }
+
+    private static void fibonachi()
+    {
+
+        int recebe, result = 0, anterior = 0, atual = 1;
+        Console.WriteLine("Digite o numero da sequencia: ");
+        recebe = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < recebe; i++)
+        {
+            result = anterior + atual;
+            anterior = atual;
+            atual = result;
+            if (atual == recebe)
+            {
+                Console.WriteLine("{0} Pertence", result);
+                break;
+            }
+        }
+        if (atual != recebe)
+        {
+            Console.WriteLine("{0} Não pertence", atual);
+        }
+    }
+}
